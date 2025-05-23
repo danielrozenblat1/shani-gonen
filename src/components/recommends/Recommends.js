e6eab96154b4cda054ed135908a7540346dc85ff
daniel rozenblat <danielroz12345@gmail.com>
2025-05-23 12:18:56 +0300

    newRecommend

--- src/components/recommends/Recommends.js
+++ src/components/recommends/Recommends.js
@@ -26,7 +26,7 @@ import result21 from "../../images/שני גונן המלצות 21.png";
 import result22 from "../../images/שני גונן המלצות 22.png";
 import result23 from "../../images/שני גונן המלצות 23.png";
 import result24 from "../../images/שני גונן המלצות 24.png";
-
+import result25 from "../../images/שני גונן המלצות 25.png";
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -88,7 +88,7 @@ const Recommends = () => {
   const images = [
     result5, result6, result7, result8, result9, result3, result4, 
     result10, result11, result12, result13, result14, result15, result16, 
-    result17, result18, result19, result20, result1, result2,result21,result22,result23,result24
+    result17, result18, result19, result20, result1, result2,result21,result22,result23,result24,result25
   ];
 
   return (
