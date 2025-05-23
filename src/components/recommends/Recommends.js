0545de22bc4be5c7e04a779b0436f93eec47c8ff
daniel rozenblat <danielroz12345@gmail.com>
2025-05-23 12:20:41 +0300

    newRecommendd

--- src/components/recommends/Recommends.js
+++ src/components/recommends/Recommends.js
@@ -86,9 +86,9 @@ const Recommends = () => {
   };
 
   const images = [
-    result5, result6, result7, result8, result9, result3, result4, 
+   result25, result5, result6, result7, result8, result9, result3, result4, 
     result10, result11, result12, result13, result14, result15, result16, 
-    result17, result18, result19, result20, result1, result2,result21,result22,result23,result24,result25
+    result17, result18, result19, result20, result1, result2,result21,result22,result23,result24
   ];
 
   return (
